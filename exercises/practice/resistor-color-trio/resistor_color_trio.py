6a023b4e9998f61176f7eae8977b5053ac257be4
dritu1993 <das.rituparnaa.1993@gmail.com>
2025-12-12 18:33:02 -0500

    Initialize color_table for resistor color values

--- exercises/practice/resistor-color-trio/resistor_color_trio.py
+++ exercises/practice/resistor-color-trio/resistor_color_trio.py
@@ -1,2 +1,9 @@
+color_table = pd.DataFrame({color : [black, brown, red, orange, yellow, green, blue, violet, grey, white],
+                            values : [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    )
+
 def label(colors):
-    pass
+    R = 0
+    for color in colors:
+        v = color_table.loc['color']
+        R+= 10
